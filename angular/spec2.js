d4a8b4a5320dfb6e964b526da0be908fc4ccfd43
Ferias <christian.beilborn@gmx.de>
2019-05-08 14:18:38 +0200

    Testfall mit epocheconverter V1.2 - Button Problem gelöst (nicht optimal) - Input-Felder können manipuliert werden

--- angular/spec2.js
+++ angular/spec2.js
@@ -1,63 +1,102 @@
-describe('Protractor Demo App', function() {
+describe('Epochconverter with protractor', function() {
 
 	// ########## Mein ToDo ##########
-	// Convert epoch to human readable date and vice versa => Manipulieren und vergleichen
 	// GMT zu Local time wechseln
-	// Button klicken und Timestamp auslesen
-	// Epoch dates for the start and end of the year/month/day => Manipulieren und vergleichen
+	// Timestamp auslesen
 	// Convert seconds to days, hours and minutes => Manipulieren und vergleichen
 	// ###############################
 
 	// Datumformat
 	var jetzt = new Date();
-	h = jetzt.getHours();
-	m = jetzt.getMinutes();
+	year = jetzt.getYear();
+	month = jetzt.getMonth();
+	day = jetzt.getDay();
+	hours = jetzt.getHours();
+	minutes = jetzt.getMinutes();
+	seconds = jetzt.getSeconds();
 	
 	// Variablen für Testen
-	var timestamp_year = element(by.css("td[id=br-d1] input[id=ycinput]"));
-	var timestamp_hours = element(by.name("hh"));
-	var timestamp_minutes = element(by.name("mn"));
-	var timestamp = element(by.name("hf-result"));
-	var timestamp_button = element.all(by.buttonText("uman date to Timestamp"));
-	// var timestamp_button = element.all(by.css(".tool hf-table")).all(by.css(".button")); --> BACKUP
+	var ymdhms_timestamp_year = element(by.id("hf-d1")).element(by.className("dateform year"));
+	var ymdhms_timestamp_month = element(by.id("hf-d2")).element(by.className("dateform month"));
+	var ymdhms_timestamp_day = element(by.id("hf-d3")).element(by.className("dateform day"));
+	var ymdhms_timestamp_hours = element(by.name("hh"));
+	var ymdhms_timestamp_minutes = element(by.name("mn"));
+	var ymdhms_timestamp_seconds = element(by.name("ss"));
+	var ymdhms_timestamp_button = element(by.buttonText("Human date to Timestamp"));
+	// var ymdhms_timestamp_button = element(by.css("tool hf-table")).element(by.buttonText("Human date to Timestamp")); Funktioniert nicht
+	var ymdhms_timestamp_result = element(by.id("hf")).element(by.id("hf-result"));
+
+
+
+
+//	var ymdhms_timestamp_year = element(by.css("td[id=br-d1] input[id=ycinput]"));
+	var convert_button = element(by.buttonText("Convert"));
+
+	// var ymdhms_timestamp_button = element.all(by.css(".tool hf-table")).all(by.css(".button")); --> BACKUP
 
     beforeEach(function () {
         browser.waitForAngularEnabled(false);
         browser.get('https://www.epochconverter.com/');
     });
 
+/*
+	// DONE!
 	it('should check year ', () => {
-		expect(timestamp_year.getAttribute('value')).toEqual('2019');
+		expect(ymdhms_timestamp_year.getAttribute('value')).toEqual("" + (1900+year));
+		expect(ymdhms_timestamp_hours.getAttribute('value')).toEqual("" + (hours-2));
+		expect(ymdhms_timestamp_minutes.getAttribute('value')).toEqual("" + minutes);
+		console.log('\nYear: ' + (1900+year) + '\nMonth: ' + (month) + '\nDay: ' + (day) + '\nTime: ' + (hours) + ':' + minutes);
     });
-
-	it('should check hours ', () => {
-		expect(timestamp_hours.getAttribute('value')).toEqual("" + (h-2));
+*/
+	// Rückgabe prüfen
+	it('manipulate date & time and check ', () => {
+		/*
+		ymdhms_timestamp_year.clear();
+		ymdhms_timestamp_year.getAttribute('value').sendKeys('1234');
+		ymdhms_timestamp_month.clear();
+		ymdhms_timestamp_month.getAttribute('value').sendKeys('11');
+		ymdhms_timestamp_day.clear();
+		ymdhms_timestamp_day.getAttribute('value').sendKeys('22');
+		ymdhms_timestamp_hours.clear();
+		ymdhms_timestamp_hours.getAttribute('value').sendKeys('22');
+		ymdhms_timestamp_minutes.clear();
+		ymdhms_timestamp_minutes.getAttribute('value').sendKeys('11');
+		ymdhms_timestamp_seconds.clear();
+		ymdhms_timestamp_seconds.getAttribute('value').sendKeys('34');
+		*/
+		ymdhms_timestamp_button.click();
+		browser.sleep(2000);
+		//expect(ymdhms_timestamp_result.XYZ.toEqual('2019'));
+		console.log(ymdhms_timestamp_result.getAttribute('value'));
+		//browser.sleep(5000);
+		
+		
+		
+		//console.log('\nYear: ' + ('1234') + '\nMonth: ' + ('11') + '\nDay: ' + ('22') + '\nTime: ' + ('22') + ':' + '11');
     });
-
-	it('should check minutes ', () => {
-		expect(timestamp_minutes.getAttribute('value')).toEqual("" + m);
-    });
-	
-	// Click on button --> It didn't work yet!	
-	it('timestamp auswertung auslesen ', () => {
-		timestamp_button.click();
-	//	browser.sleep(5000)
-		// console.log(timestamp);	
+/*	
+		browser.sleep(2000)
+		convert_button.click();
+		browser.sleep(2000)
 	});
 
-	it('should check year ', () => {
+	it('should check year ', ()	 => {
 		//expect(element(by.id('ycinput')).getAttribute('value')).toEqual('2019');
-		expect(timestamp_year.getAttribute('value')).toEqual('2019');
+		expect(ymdhms_timestamp_year.getAttribute('value')).toEqual('2019');
     });
+
+
 
 	// Manipulate value of input --> It didn't work yet!
 	it('give number 1234 in field ycinput', () => {
-		timestamp_year.clear();
-		timestamp_year.sendKeys('1234');
+		ymdhms_timestamp_year.clear();
+		ymdhms_timestamp_year.sendKeys('1234');
+		browser.sleep(2000)
     });
 	
 	// Check manipulated value of input --> It didn't work yet!
     it('check year', () => {
-		expect(timestamp_year.getAttribute('value')).toEqual('');
+		expect(ymdhms_timestamp_year.getAttribute('value')).toEqual('');
     });
+*/
 });
