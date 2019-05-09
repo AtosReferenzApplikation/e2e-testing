ad6e7a0b17094108f9c83eba1468f5a5cdb92a0d
Ferias <christian.beilborn@gmx.de>
2019-05-09 16:01:59 +0200

    Testfall mit epocheconverter V1.4 - moment.js integrieren - 1 zusätzlich Test case (TC 4.1) - Buttonklick bei TC 1.2 verbessert

--- angular/spec.js
+++ angular/spec.js
@@ -1,22 +1,21 @@
-describe('Epochconverter with protractor', function() {
+var moment = require('moment');
 
+describe('Epochconverter with protractor -', function() {
 	// ########## Mein ToDo ##########
 	// GMT zu Local time wechseln
-	// Timestamp auslesen
-	// Convert seconds to days, hours and minutes => Manipulieren und vergleichen
-	// Moment.js einbauen
+	// Timestamp to Human date fehlt noch
+	// Jeweils Rückkgabe nach Buttonklick auslesen
 	// ###############################
 
 	// Datumformat
-	var jetzt = new Date();
-	year = jetzt.getYear();
-	month = jetzt.getMonth()+1;
-	day = jetzt.getDay()+5;
-	hours_utc = jetzt.getHours()-2;
-	hours = jetzt.getHours();
-	minutes = jetzt.getMinutes();
-	seconds = jetzt.getSeconds();
-	
+	year = moment().format('Y');
+	month = moment().format('M');
+	day = moment().format('D');
+	hours_utc = moment().utc().format('H');
+	hours = moment().format('H');
+	minutes = moment().format('m');
+	seconds = moment().format('s');
+
 	// Variablen für Testen 
 	var ymdhms_timestamp_year = element(by.id("hf-d1")).element(by.className("dateform year"));
 	var ymdhms_timestamp_month = element(by.id("hf-d2")).element(by.className("dateform month"));
@@ -24,16 +23,15 @@ describe('Epochconverter with protractor', function() {
 	var ymdhms_timestamp_hours = element(by.name("hh"));
 	var ymdhms_timestamp_minutes = element(by.name("mn"));
 	var ymdhms_timestamp_seconds = element(by.name("ss"));
-	var ymdhms_timestamp_button = element(by.buttonText("Human date to Timestamp"));
+	var ymdhms_timestamp_button = element.all(by.buttonText("Human date to Timestamp")).first();
 	// var ymdhms_timestamp_button = element(by.css("tool hf-table")).element(by.buttonText("Human date to Timestamp")); Funktioniert nicht
-	var ymdhms_timestamp_result = element(by.id("hf")).element(by.id("hf-result"));
 	var timestamp = element(by.id("fs")).element(by.id("rcinput"));
 	var timestamp_button = element(by.id("fs")).element(by.buttonText("Human date to Timestamp"));
 	var convert_year = element(by.id("br-d1")).element(by.className("dateform year"));
 	var convert_month = element(by.id("br-d2")).element(by.className("dateform month"));
 	var convert_button = element(by.buttonText("Convert"));
-	var convert_sdhm = element(by.id("fs")).element(by.id("scinput"));
-	var convert_sdhm_button = element(by.buttonText("Seconds to days, hours, minutes"));
+	var convert_sdhm = element(by.id("tc")).element(by.id("scinput"));
+	var convert_sdhm_button = element(by.id("tc")).element(by.buttonText("Seconds to days, hours, minutes"));
 	// var convert_year = element(by.css("td[id=br-d1] input[id=ycinput]"));
 
 	// Test case Convert epoch to human readable date and vice versa
@@ -44,14 +42,14 @@ describe('Epochconverter with protractor', function() {
 
 	// DONE!
 	it('TC 1.1. Check date & time', () => {
-		expect(ymdhms_timestamp_year.getAttribute('value')).toEqual("" + (1900+year));
+		expect(ymdhms_timestamp_year.getAttribute('value')).toEqual("" + (year));
 		expect(ymdhms_timestamp_hours.getAttribute('value')).toEqual("" + (hours_utc));
 		expect(ymdhms_timestamp_minutes.getAttribute('value')).toEqual("" + minutes);
 		//console.log('\nYear: ' + (1900+year) + '\nMonth: ' + (month) + '\nDay: ' + (day) + '\nTime: ' + (hours_utc) + ':' + minutes);
     });
-	// TODO Ausgabe muss noch ausgelesen werden 
-	it('TC 1.2. Manipulate date & time and check', () => {
-		
+
+	// TODO Ausgabe muss noch ausgelesen werden
+	it('TC 1.2. Manipulate date & time and check', async function() {
 		ymdhms_timestamp_year.clear();
 		ymdhms_timestamp_year.getAttribute('value').sendKeys('1234');
 		ymdhms_timestamp_month.clear();
@@ -63,60 +61,44 @@ describe('Epochconverter with protractor', function() {
 		ymdhms_timestamp_minutes.clear();
 		ymdhms_timestamp_minutes.getAttribute('value').sendKeys('11');
 		ymdhms_timestamp_seconds.clear();
-		ymdhms_timestamp_seconds.getAttribute('value').sendKeys('34');
+		ymdhms_timestamp_seconds.getAttribute('value').sendKeys('34');		
 		ymdhms_timestamp_button.click();
-		//browser.sleep(2000);
-		//expect(ymdhms_timestamp_result.XYZ.toEqual('1111'));
-		console.log('Hier: ' + ymdhms_timestamp_result.innerText);
 		//browser.sleep(5000);
+		//expect(ymdhms_timestamp_result.XYZ.toEqual('1111'));
+		//var ymdhms_timestamp_result = element(by.id("hf-result"));
+		//console.log('Hier: ' + ymdhms_timestamp_result.innerText);
     });
-	
+
 	// TODO Ausgabe muss noch ausgelesen werden
-	it('TC 2.1 manipulate date & time and check', () => {
+	it('TC 2.1 manipulate date & time and check', async function() {
 		expect(timestamp.getAttribute('value')).toEqual('Wed, 08 May 2019 15:16:02 GMT'); // It will be always wrong, just ignore this error.
 		timestamp.clear();
 		timestamp.getAttribute('value').sendKeys('Wed, 01 May 2019 15:16:02 GMT');
-		timestamp_button.click();
+		browser.sleep(5000);
+		await timestamp_button.click();
     });
 
 	// Epoch dates for the start and end of the year/month/day
 	// TODO Ausgabe muss noch ausgelesen werden
-	it('TC 3.1. manipulate date & time and check', () => {
-		expect(convert_year.getAttribute('value')).toEqual("" + (1900+year));
+	it('TC 3.1. manipulate date & time and check', async function() {
+		expect(convert_year.getAttribute('value')).toEqual("" + (year));
 		expect(convert_month.getAttribute('value')).toEqual("" + (month));
 		convert_year.clear();
 		convert_year.getAttribute('value').sendKeys('2000');
 		convert_month.clear();
 		convert_month.getAttribute('value').sendKeys('12');
-		convert_button.click();
+		browser.sleep(5000);
+		await convert_button.click();
     });
 
-/*
 	// Convert seconds to days, hours and minutes
-	// TODO ...
-	it('TC 4.1. manipulate date & time and check', () => {
-		//...
-    });
-
-	//console.log('\nYear: ' + ('1234') + '\nMonth: ' + ('11') + '\nDay: ' + ('22') + '\nTime: ' + ('22') + ':' + '11');
-
-	it('should check year ', ()	 => {
-		//expect(element(by.id('ycinput')).getAttribute('value')).toEqual('2019');
-		expect(ymdhms_timestamp_year.getAttribute('value')).toEqual('2019');
-    });
-
-
-
-	// Manipulate value of input --> It didn't work yet!
-	it('give number 1234 in field ycinput', () => {
-		ymdhms_timestamp_year.clear();
-		ymdhms_timestamp_year.sendKeys('1234');
-		browser.sleep(2000)
-    });
-	
-	// Check manipulated value of input --> It didn't work yet!
-    it('check year', () => {
-		expect(ymdhms_timestamp_year.getAttribute('value')).toEqual('');
-    });
-*/
+	// TODO Buttonklick funktioniert nicht & Ausgabe auslesen
+	it('TC 4.1. manipulate seconds and check', async function() {
+		expect(convert_sdhm.getAttribute('value')).toEqual('90061');
+		convert_sdhm.clear();
+		convert_sdhm.getAttribute('value').sendKeys('20000');
+		browser.sleep(5000);
+		await convert_sdhm_button.click();
+	});
 });
+
