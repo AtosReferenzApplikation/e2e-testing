6b8c3aee3129055466c4529d3423216c056fd398
Ferias <christian.beilborn@gmx.de>
2019-06-05 16:43:50 +0200

    - Promise bei Datei ymdhms_timestamp.ts eingebaut - Erweiterung bei .gitignore

--- epochconverter/ymdhms_timestamp.ts
+++ epochconverter/ymdhms_timestamp.ts
@@ -1,14 +1,24 @@
 import {element, by, ElementFinder} from 'protractor';
 
 export class YearMonthDayHoursMinutesSeconds {
-	public ymdhms_timestamp_year: ElementFinder = element(by.id("hf-d1")).element(by.className("dateform year"));
-	public ymdhms_timestamp_month: ElementFinder = element(by.id("hf-d2")).element(by.className("dateform month"));
-	public ymdhms_timestamp_day: ElementFinder = element(by.id("hf-d3")).element(by.className("dateform day"));
-	public ymdhms_timestamp_hours: ElementFinder = element(by.name("hh"));
-	public ymdhms_timestamp_minutes: ElementFinder = element(by.name("mn"));
-	public ymdhms_timestamp_seconds: ElementFinder = element(by.name("ss"));
-	public ymdhms_timestamp_button: ElementFinder = element.all(by.buttonText("Human date to Timestamp")).first();
-	
+	public ymdhms_timestamp_year: ElementFinder;
+	public ymdhms_timestamp_month: ElementFinder;
+	public ymdhms_timestamp_day: ElementFinder;
+	public ymdhms_timestamp_hours: ElementFinder;
+	public ymdhms_timestamp_minutes: ElementFinder;
+	public ymdhms_timestamp_seconds: ElementFinder;
+	public ymdhms_timestamp_button: ElementFinder;
+
+	constructor() {
+		this.ymdhms_timestamp_year = element(by.id("hf-d1")).element(by.className("dateform year")),
+		this.ymdhms_timestamp_month = element(by.id("hf-d2")).element(by.className("dateform month")),
+		this.ymdhms_timestamp_day = element(by.id("hf-d3")).element(by.className("dateform day")),
+		this.ymdhms_timestamp_hours = element(by.name("hh")),
+		this.ymdhms_timestamp_minutes = element(by.name("mn")),
+		this.ymdhms_timestamp_seconds = element(by.name("ss")),
+		this.ymdhms_timestamp_button = element.all(by.buttonText("Human date to Timestamp")).first()
+	}
+
 	public async perform(year: string, month: string, day: string, hours: string, minutes: string, seconds: string) {
 		await this.ymdhms_timestamp_year.clear();
 		this.ymdhms_timestamp_year.sendKeys(year);
@@ -24,4 +34,17 @@ export class YearMonthDayHoursMinutesSeconds {
 		this.ymdhms_timestamp_seconds.sendKeys(seconds);
 		await this.ymdhms_timestamp_button.click();
 	}
+	//public async ConvertedValue: Promise<string>{}
+
+	public CoP: number = 1;
+	let PromiseTest = new Promise((resolve, reject) => {
+		setTimeout(function() {
+			resolve(this.CoP);
+		}, 1000);
+	});
+	PromiseTest.then((val) => {
+		console.log('Done!');
+	}).catch((reason) => {
+		console.log('Error ('+reason+') here.');
+	});
 }
